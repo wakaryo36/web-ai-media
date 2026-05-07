ce8b30e8dfce4e4cda0c62da7927e293c87d3083
wakaryo36 <wakaryo36@gmail.com>
2026-05-07 22:35:28 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -52,3 +52,32 @@ document.querySelectorAll(".track-school").forEach(btn => {
         trackClick("school_click");
     });
 });
+
+// ヘッダー影追加
+window.addEventListener("scroll", () => {
+    const header = document.querySelector(".site-header");
+
+    if (window.scrollY > 10) {
+        header.classList.add("header-scrolled");
+    } else {
+        header.classList.remove("header-scrolled");
+    }
+});
+
+// TOPへ戻るボタン
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        backToTop.style.display = "block";
+    } else {
+        backToTop.style.display = "none";
+    }
+});
+
+backToTop.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
